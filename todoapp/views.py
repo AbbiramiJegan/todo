c2e778a6b073d2b566d3579c85300ada00d3bda4
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-08-11 06:10:08 +0800

    Implement views for task creation, deletion, and status update in To-Do list application.

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import todo
-
-# Create your views here.
 
 def home(request):
     if request.method == 'POST':
         task = request.POST.get('task')
-        new_todo = todo(todo_name=task)
-        new_todo.save()
+        if task:  # Ensure task is not empty
+            todo.objects.create(todo_name=task)
 
-    return render(request, 'todoapp/todo.html', {})
+    context = {
+        'todos': todo.objects.all()
+    }
+    return render(request, 'todoapp/todo.html', context)
+
+def delete_todo(request, id):
+    if request.method == 'POST':
+        get_object_or_404(todo, id=id).delete()
+    return redirect('home')
+
+def mark_finished(request, id):
+    if request.method == 'POST':
+        todo_item = get_object_or_404(todo, id=id)
+        todo_item.status = True
+        todo_item.save()
+    return redirect('home')
