32ef8bce3ea5b33bd647cb3f2db080398fb2d1c2
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-08-11 06:11:38 +0800

    Added URL patterns for deleting and marking To-Do items as finished

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
+    path('', views.home, name='home'),  # Home page
+    path('delete/<int:id>/', views.delete_todo, name='delete_todo'),  # Delete a todo item
+    path('finish/<int:id>/', views.mark_finished, name='mark_finished'),  # Mark a todo item as finished
 ]
