41e967ee581e9583bacd17e45506ac5bbe94494e
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-08-11 03:16:26 +0800

    Add 'home' view to handle creating new todo items

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import todo
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        task = request.POST.get('task')
+        new_todo = todo(todo_name=task)
+        new_todo.save()
+
     return render(request, 'todoapp/todo.html', {})
